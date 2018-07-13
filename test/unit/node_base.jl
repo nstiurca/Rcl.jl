ec252249ed7fac028fb0485703396031b6ac91e5
Nicu Stiurca <supernic2002@gmail.com>
2018-07-12 20:09:00 -0500

    Switch @assert back to @test
    It seems to work now with julia-0.7.0-beta and ROS 2 bouncy.

--- test/unit/node_base.jl
+++ test/unit/node_base.jl
@@ -6,29 +6,29 @@ using Test
 
 @testset "Node" begin
     # initialize
-    @assert !rcl.ok()
+    @test !rcl.ok()
     rcl.init()
-    @assert rcl.ok()
+    @test rcl.ok()
 
     nb = rcl.NodeBase("test_node")
-    @assert rcl.isvalid(nb)
+    @test rcl.isvalid(nb)
 
     # finalizing it shouldn't cause errors
     finalize(nb)
-    @assert !rcutils_error_is_set()
+    @test !rcutils_error_is_set()
 
     # But checking if the node is still valid apparently does set the error
     # TODO: file a bug about this with the rcl folks?
-    @assert !rcl.isvalid(nb)
-    @assert rcutils_error_is_set()
+    @test !rcl.isvalid(nb)
+    @test rcutils_error_is_set()
     # The error was expected so reset it
     rcutils_reset_error()
-    @assert !rcutils_error_is_set()
+    @test !rcutils_error_is_set()
 
     # shutdown
-    @assert rcl.ok()
+    @test rcl.ok()
     rcl.shutdown()
-    @assert !rcl.ok()
+    @test !rcl.ok()
 end
 
 # end # module TestRclBase
