13ba7d46fc7f154572b09255f76ff058e8b8f16e
Nicu Stiurca <supernic2002@gmail.com>
2019-03-08 04:33:53 -0600

    Sort out C structs with fixed size strings and checkcall()

--- gen/LibRcl.jl
+++ gen/LibRcl.jl
@@ -39,12 +39,43 @@ struct RclError <: Exception
     msg :: AbstractString
 end
 
+struct RcutilsErrorState <: Exception
+    message :: String
+    file :: String
+    line_number :: Int
+end
+
+# Base.show(io::IO, err::RclError) = print(io, "RclError($(err.code), \"$(err.msg)\")")
+
+@inline function fixed_size_string(s::T, field_idx, expected_field_name) where T
+    U = fieldtype(T, field_idx)
+    @assert fieldname(T, field_idx) == expected_field_name fieldname(T, field_idx)
+    @assert U <: NTuple U
+    @assert eltype(U) == Cchar eltype(U)
+    ptr = Ptr{Cchar}(pointer_from_objref(s) + fieldoffset(T, field_idx))
+    len = ccall(:strnlen, Csize_t, (Cstring, Csize_t), ptr, U.size)
+    unsafe_string(ptr, len)
+end
+
+Base.string(err_str::rcutils_error_string_t) = fixed_size_string(err_str, 1, :str)
+Base.show(io::IO, err_str::rcutils_error_string_t) = print(io, "$(typeof(err_str))(\"$(string(err_str))\")")
+
+message(err_state::rcutils_error_state_t) = fixed_size_string(err_str, 1, :message)
+file(err_state::rcutils_error_state_t) = fixed_size_string(err_str, 2, :file)
+line_number(err_state::rcutils_error_state_t) = Int(err_state.line_number)
+
+Base.convert(::Type{RcutilsErrorState}, err_state::rcutils_error_state_t) = RcutilsErrorState(
+    message(err_state), file(err_state), line_number(err_state)
+)
+
+Base.show(io::IO, err_state::rcutils_error_state_t) = show(io, convert(RcutilsErrorState, err_state))
+
 function checkcall(code :: rcl_ret_t; suppress_throw=false)
     code == RCL_RET_OK && return nothing
 
     msg = rcutils_get_error_string()
-    @assert msg != Cstring(C_NULL)
-    err = RclError(code, unsafe_string(msg))
+    str = string(msg)
+    err = RclError(code, str)
 
     rcutils_reset_error()
 
